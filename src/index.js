f7a6879de63b13840788cfd2889c869e834291c4
chendadou <463363269@qq.com>
2023-02-21 18:39:19 +0800

    add style-loader, css-loader, less-loader and sass-loader

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import './style.css';
+import './style.less';
+import './style.scss';
+
 const App = () => (
-  <div>Hello World.</div>
+  <div id='test' className='less-test'>
+    <span className='font-40'>Hello World</span>
+  </div>
 )
 
 ReactDom.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const path = require('path');
 const dir = path.resolve(__dirname, 'src');
 
-// const isDevMode = process.env.NODE_ENV == 'development';
+const isDevMode = process.env.NODE_ENV == 'development';
 
 module.exports = {
   mode: 'development',
@@ -24,14 +25,43 @@ module.exports = {
         test: /\.m?(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
         }
-      }
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          // compiles Less to CSS
+          isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'less-loader',
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // 将 JS 字符串生成为 style 节点
+          isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          // 将 CSS 转化成 CommonJS 模块
+          'css-loader',
+          // 将 Sass 编译成 CSS
+          'sass-loader',
+        ],
+      },
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: `${dir}/index.html`,
     }),
+    new MiniCssExtractPlugin(),
   ]
 }
